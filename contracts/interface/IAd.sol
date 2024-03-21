2900e6e2d5a1f0e8de5ceab7f84a810774d20128
WaterSo0910 <jacky950141@gmail.com>
2024-03-22 02:26:38 +0800

    Remove unused interface files

--- contracts/interface/IAd.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
-
-interface IAdboost {
-    function boost(int tokenId, address nextBooster) external;
-    function mintBoostToken(int amount) external;
-    function burnBoostToken(int amount) external;
-    function getBoostTokenBalance() external view returns (int);
-} 
--- contracts/interface/IAdboost.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.24;
-
-interface IAdboost {
-    function createAd() external;
-
-    function chargeAd() external;
-} 
