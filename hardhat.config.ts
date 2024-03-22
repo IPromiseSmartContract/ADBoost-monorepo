a41ef1bd7d649689711c3322f7d156dd5a374101
WaterSo0910 <jacky950141@gmail.com>
2024-03-22 17:14:57 +0800

    Update hardhat.config.ts with Solidity version and network settings

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,23 @@ import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.24",
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  networks: {
+    hardhat: {
+      forking: {
+        url: "https://eth-sepolia.g.alchemy.com/v2/zF6xioFDOk1zHKWUGk-bmtRZmxxz4Vjb",
+        blockNumber: 5535029,
+      },
+    },
+  },
 };
 
 export default config;
